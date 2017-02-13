2ea55382855b50b8ae16cd7148455c1d90fd1d20
Nesta Sanchez <Nesta Sanchez>
2017-02-13 13:31:25 +0000

    Initial commit

---
+++ src/temperatura.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class temperatura {
+
+	public static void main(String[] args) {
+		int[] temperaturas= new int [10];
+		int valor;
+		Scanner sc = new Scanner(System.in);
+
+		for(int i=0; i<temperaturas.length; i++){
+			System.out.println("Introduzca las temperaturas");
+			valor = sc.nextInt();
+			temperaturas[i]=valor;
+			fdgdfgdgdfgdf
+					
+		}
+		//El arrays to string nos muestra como un String
+		System.out.println("Las temperaturas son: " + Arrays.toString(temperaturas));
+	}
+
+}
